7b57fd5b5721b74fce96e50b35fa77fe66104aa2
galactus-clb <chanakabalasuriya123@gmail.com>
2026-03-31 13:13:52 +0530

    API: Refactor

--- api/src/features/product/product.repository.ts
+++ api/src/features/product/product.repository.ts
@@ -3,3 +3,11 @@ import prisma from "@/lib/prisma/prisma.ts"
 export const findAll = async () => {
     return await prisma.product.findMany()
 }
+
+export const findByName = async (name: string)=>{
+    return await prisma.product.findUnique({
+        where: {
+            id: "12"
+        }
+    })
+}
