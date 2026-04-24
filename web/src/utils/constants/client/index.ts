7b28d45a89cfc1c42fc3e479541ccd06eb3aba3c
galactus-clb <chanakabalasuriya123@gmail.com>
2026-04-24 16:21:16 +0530

    WEB: Fixed the environment variable parsing issue because of need to explicit keys for client and server schemas

--- web/src/utils/constants/client/index.ts
+++ web/src/utils/constants/client/index.ts
@@ -1,6 +1,8 @@
 import { clientEnvSchema } from "./schema"
 
-const env = clientEnvSchema.parse(process.env)
+const env = clientEnvSchema.parse({
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+})
 
 export const constants = {
     API: {
--- web/src/utils/constants/server/index.ts
+++ web/src/utils/constants/server/index.ts
@@ -2,7 +2,13 @@ import "server-only"
 
 import { serverEnvSchema } from "./schema"
 
-const env = serverEnvSchema.parse(process.env)
+const env = serverEnvSchema.parse({
+    API_BASE_URL: process.env.API_BASE_URL,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+})
 
 export const constants = {
     API: {
