b4c2054a12ab2ca88f4688e8d1cfbc3992dd0e8f
galactus-clb <chanakabalasuriya123@gmail.com>
2026-04-29 14:35:55 +0530

    [WEB]: SSG launch details pages using `generateStaticParams`

--- web/src/app/(pages)/(home)/launch/[id]/page.tsx
+++ web/src/app/(pages)/(home)/launch/[id]/page.tsx
@@ -1,7 +1,26 @@
 import { Suspense } from "react";
 
+import { getDailyProducts, getNewProducts, getWeeklyProducts } from "@/features/product";
 import { LaunchPageContainer } from "@/features/views/home/launch";
 import type { LaunchPageContainerProps } from "@/features/views/home/launch";
+import { ProductIdParam } from "@/models/product.schema";
+
+type ProductStaticParams = Record<keyof ProductIdParam, string>;
+
+export async function generateStaticParams(): Promise<ProductStaticParams[]> {
+    const [daily, weekly] = await Promise.all([
+        getNewProducts(),
+        getDailyProducts(),
+        getWeeklyProducts(),
+    ]);
+
+    const ids = new Set([
+        ...daily.data?.map(p => p.id) ?? [],
+        ...weekly.data?.map(p => p.id) ?? [],
+    ]);
+
+    return Array.from(ids).map(id=>({id: String(id)}));
+};
 
 export default async function LaunchItemPage({
     params,
