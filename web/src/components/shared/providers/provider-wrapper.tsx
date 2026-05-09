dfd084c88536c9fa37d19964a40287f8fdfdfce7
galactus-clb <chanakabalasuriya123@gmail.com>
2026-05-09 12:29:20 +0530

    [WEB]: Integrate RscBoundaryProvider for improved error handling in ProviderWrapper

--- web/src/components/shared/providers/provider-wrapper.tsx
+++ web/src/components/shared/providers/provider-wrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { RscBoundaryProvider } from "@rsc-boundary/next";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode, useState } from 'react';
@@ -17,11 +18,13 @@ export default function ProviderWrapper({ children }: { children: ReactNode }) {
     }));
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                {children}
-            </AuthProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <RscBoundaryProvider>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    {children}
+                </AuthProvider>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </RscBoundaryProvider>
     );
 }
