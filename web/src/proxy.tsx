07887f36a4c47a23775e4c584dfae9d73721ff8e
galactus-clb <chanakabalasuriya123@gmail.com>
2026-05-21 19:05:02 +0530

    [WEB]: Removed a unnecessary commenteds line

--- web/src/proxy.tsx
+++ web/src/proxy.tsx
@@ -60,7 +60,4 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|_next/webpack-hmr|favicon.ico).*)'],
-  // matcher: [
-  //   '/((?!_next/static|_next/image|_next/webpack-hmr|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  // ],
 }
