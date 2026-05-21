3f7e9178fa0e1d4f92b998af324009ec6e3d07fd
galactus-clb <chanakabalasuriya123@gmail.com>
2026-05-21 08:12:34 +0530

    [INFRA]: Updated ECS API image tag to latest version

--- infra/main.tf
+++ infra/main.tf
@@ -98,7 +98,7 @@ module "ecs" {
   web_env_cloudfront_domain = module.cloudfront.cloudfront_domain_name
   web_env_s3_bucket_name = module.s3.s3_bucket_name
 
-  ecs_api_image = "566895563031.dkr.ecr.us-east-1.amazonaws.com/launchzap-api:202605201840"
+  ecs_api_image = "566895563031.dkr.ecr.us-east-1.amazonaws.com/launchzap-api:202605210750"
   api_port = var.api_port
   target_group_api_arn = module.alb.alb_tg_api_arn
   ecs_api_sg_ids = [module.security_groups.ecs_api_sg_id]
