c6d903a55a1ae70c96ba37fb1a5cbd12403e1ee7
galactus-clb <chanakabalasuriya123@gmail.com>
2026-06-02 13:05:00 +0530

    [API]: Added missing redis client instance initialization using a boostrap function on server initialization

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,3 @@
-
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,21 @@
-// server.ts
 import app from './app.ts';
+import { logger } from './lib/logger/index.ts';
+import { getRedisClient } from './lib/redis/redis-client.ts';
+
 const PORT = process.env.PORT || 4000;
+
+async function boostrap() {
+    getRedisClient(
+        process.env.REDIS_CLIENT_URL
+            ? { url: process.env.REDIS_CLIENT_URL, host: '', port: 6379 }
+            : { host: process.env.REDIS_HOST!, port: 6379 }
+    );
+};
+
+await boostrap();
+
 app.listen(PORT, () => {
     const url = `http://localhost:${PORT}`;
-    console.log(`Server running on port ${PORT} 🚀`);
-    console.log(`URL: ${url}`);
+    logger.info(`Server running on port ${PORT} 🚀`);
+    logger.info(`URL: ${url}`);
 });
