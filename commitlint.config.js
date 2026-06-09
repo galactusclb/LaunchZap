3e7a60aad83b75c732f6cd782be399b471aea286
galactus-pc <chanakabalasuriya123@gmail.com>
2026-06-09 10:04:51 +0530

    chore(config): fix commit lint not working issue

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', ['api', 'web', 'infra', 'cicd', 'db', 'config', 'deps']],
-    'scope-empty': [2, 'never'],
-  },
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [2, 'always', ['api', 'web', 'infra', 'cicd', 'db', 'config', 'deps']],
+        'scope-empty': [2, 'never'],
+    },
 };
