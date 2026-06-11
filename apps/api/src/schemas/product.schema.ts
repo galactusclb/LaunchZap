5b629eba7a10d1bb5baf79fb918faa8b24154e2c
galactus-pc <chanakabalasuriya123@gmail.com>
2026-06-11 11:26:55 +0530

    feat(api): update zod schemas based on launch and product prisma models

--- apps/api/src/schemas/product.schema.ts
+++ apps/api/src/schemas/product.schema.ts
@@ -1,8 +1,10 @@
 import z from 'zod';
 
 import { trimmedString } from '@/lib/zod/extras';
-import { ProductStatus } from '@/prisma/client';
 import { userSchema } from '@/schemas/user.schema';
+import { constants } from '@/utils/constant';
+
+const productStatusList = constants.productStatus;
 
 export const baseProductSchema = z.object({
     name: trimmedString.min(1).max(100),
@@ -10,16 +12,14 @@ export const baseProductSchema = z.object({
     description: trimmedString.max(1000).optional(),
     websiteUrl: z.string().url(),
     logoUrl: z.string().url().optional(),
-    launchDate: z.coerce.date(),
 });
 
 export const productResponseSchema = baseProductSchema.extend({
     id: z.number(),
     createdAt: z.date(),
     updatedAt: z.date(),
-    status: z.nativeEnum(ProductStatus),
+    status: z.enum(productStatusList),
     maker: userSchema.pick({ id: true, name: true }),
-    votesCount: z.number(),
 });
 
 export type Product = z.infer<typeof productResponseSchema>;
