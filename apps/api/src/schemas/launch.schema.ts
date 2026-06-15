3871dacff13fdd4ac6af3c595f793fbc14f1a701
galactus-clb <chanakabalasuriya123@gmail.com>
2026-06-15 15:24:54 +0530

    feat(api): add missing properties to base launch zod schema

--- apps/api/src/schemas/launch.schema.ts
+++ apps/api/src/schemas/launch.schema.ts
@@ -3,13 +3,15 @@ import z from 'zod';
 import { trimmedString } from '@/lib/zod/extras';
 import { constants } from '@/utils/constant';
 
+import { productResponseSchema } from './product.schema';
+
 const launchStatusList = constants.launchStatus;
 
 export const baseLaunchSchema = z.object({
-    name: trimmedString.min(1).max(100),
     tagline: trimmedString.min(1).max(250),
-    description: trimmedString.max(1000).optional(),
+    description: trimmedString.max(1000),
     launchDate: z.coerce.date(),
+    gallery: z.array(z.string()),
 });
 
 export const launchResponseSchema = baseLaunchSchema.extend({
@@ -18,7 +20,7 @@ export const launchResponseSchema = baseLaunchSchema.extend({
     updatedAt: z.date(),
     status: z.enum(launchStatusList),
     votesCount: z.number(),
-    productId: z.coerce.number(),
+    productId: productResponseSchema.shape.id,
 });
 
 export type Launch = z.infer<typeof launchResponseSchema>;
